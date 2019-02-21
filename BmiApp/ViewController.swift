9dc7122fd39f357859f99b8428cbcb5fc321f130
k86095 <k86095@gmail.com>
2019-02-21 16:01:06 +0800

    Initial Commit

---
+++ BmiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BmiApp
+//
+//  Created by 方錦泉 on 2019/2/21.
+//  Copyright © 2019 方錦泉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
